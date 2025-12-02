7e2f5d5e10860b33bc83967edf7ef9c59aec172f
anusha <anushakulkarni007@gmail.com>
2025-12-02 08:52:07 +0530

    save

---
+++ employee.py
@@ -0,0 +1,15 @@
+
+def employee_details(name,emp_id,department, salary):
+    result=(
+        f"Employee name:{name}\n"
+        f"Employee ID:{emp_id}\n"
+        f"Department:{department}\n"
+        f"Salary:{salary}"
+    )
+    return result
+if __name__=="__main__":
+    name = "Anusha"
+    emp_id = "01FE24BCA016"
+    department = "BCA"
+    salary = 55000
+    print(employee_details(name,emp_id,department,salary))
---
+++ test_employee.py
@@ -0,0 +1,10 @@
+from employee import employee_details
+def test_employee_details():
+    expected_output=(
+        "Employee name:Anusha\n"
+        "Employee ID:01FE24BCA016\n"
+        "Department:BCA\n"
+        "Salary:55000"
+    )
+
+    assert employee_details("Anusha", "01FE24BCA016", "BCA", 55000)==expected_output
